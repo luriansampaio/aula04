39cbf0c06ee4e04084fc7bcbadf7e76f123c911a
LURIAN OLIVEIRA SAMPAIO <lurian.sampaio@aluno.senac.rio>
2025-11-25 21:26:33 -0300

    Operadores and e or

--- aula04/exemplo01.py
+++ aula04/exemplo01.py
@@ -18,8 +18,41 @@
 #     print("Nível bom!")
 # elif pontos >= 25:
 #     print('Pontuação ruim')    
+# # else:
+
+# # Verificação de login 
+# usuario = input('Informe o usuario:').upper()
+# senha = input('Digite a senha:')
+
+# print(usuario)
+
+# if usuario == 'admin' and senha == '1234':
+#     print('O pai tá on')
 # else:
-#     print('Pontuação baixa')
+#     print('Ops! Usuário ou senha incorretos!')
+
+# # Condição para brinde
+# compra = float(input('Digite o valor da compra:'))
+# cliente_frequente = input('Cliente frequente? [S/N]').lower()
+
+# if compra >= 1000 or cliente_frequente == 's':
+#     print('Tem direito a brinde!')
+# else:
+#     print('Sem brinde!')
+
+nota = float(input('Informe a nota:'))
+frequencia = float(input('Informe a frequencia:'))
+
+if nota >= 7: 
+    if frequencia >= 75:
+        print('APROVADO!')
+    else:
+        print('Boa nota, mas reprovado por falta!')
+else:
+    print('Reprovado por nota!')
+    
+
+
 
 
 
